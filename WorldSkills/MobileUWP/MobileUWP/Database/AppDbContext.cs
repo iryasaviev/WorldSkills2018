8c611a69364efff472401071fc330bff71d18e49
ir_yasaviev <ir.yasaviev@yandex.ru>
2018-10-29 08:41:35 +0300

    Created mobile(UWP) project.

---
+++ WorldSkills/MobileUWP/MobileUWP/Database/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MobileUWP.Database.Entities;
+
+namespace MobileUWP.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlite("Filename=WorldSkillsMobile.db");
+        }
+    }
+}
---
+++ WorldSkills/MobileUWP/MobileUWP/Database/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace MobileUWP.Database.Entities
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string Name { get; set; }
+    }
+}
